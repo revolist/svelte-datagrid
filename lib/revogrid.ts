9cf2dcccd400d26333a0a86ef1079c6f052896f5
maks <mojpo4tovik@gmail.com>
2024-06-23 16:23:32 +0100

    chore(release): update versions to 4.0.12

--- lib/revogrid.ts
+++ lib/revogrid.ts
@@ -5,8 +5,4 @@ import { defineCustomElements } from '@revolist/revogrid/loader';
 
 defineCustomElements();
 import RevoGrid from './components/RevoGrid';
-import RevogrFilterPanel from './components/RevogrFilterPanel';
-import VnodeHtml from './components/VnodeHtml';
 export { RevoGrid };
-export { RevogrFilterPanel };
-export { VnodeHtml };
